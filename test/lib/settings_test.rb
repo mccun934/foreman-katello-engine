659a9ff2049b81275d62dbe61cafa722edfc064f
Ivan Necas <inecas@redhat.com>
2013-06-11 16:07:25 +0200

    Make sure the tests are independent
    In some order the settings_test was failing

--- test/lib/settings_test.rb
+++ test/lib/settings_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SettingsTest < ActiveSupport::TestCase
 
+  def setup
+    Setting::Katello.all.each(&:destroy)
+  end
+
   test "katello specific settings" do
     Setting::Katello.load_defaults
     assert_equal 'https://localhost/katello', Setting::Katello['katello_url']
