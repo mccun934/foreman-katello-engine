d7db16f345f4ae5c7463d4a2d3c344061035cf8a
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2013-10-03 15:08:24 +0100

    fixes BZ 976418: rhel-specific kickstart file uses --skip to trigger a non-interactive boot

--- db/migrate/20130416152224_add_katello_templates.rb
+++ db/migrate/20130416152224_add_katello_templates.rb
@@ -2,8 +2,13 @@ class AddKatelloTemplates < ActiveRecord::Migration
   def self.up
     ConfigTemplate.where(:name => "Katello Kickstart Default").first_or_create!( 
         :template_kind_id    => TemplateKind.find_by_name('provision').id,
-        :operatingsystem_ids => Redhat.all.map(&:id),
+        :operatingsystem_ids => Operatingsystem.where("name not like ? and type = ?", "Red Hat Enterprise Linux", "Redhat").map(&:id),
         :template            => File.read("#{ForemanKatelloEngine::Engine.root}/app/views/unattended/kickstart-katello.erb"))
+
+    ConfigTemplate.where(:name => "Katello Kickstart Default for RHEL").first_or_create!( 
+        :template_kind_id    => TemplateKind.find_by_name('provision').id,
+        :operatingsystem_ids => Redhat.where("name like ?", "Red Hat Enterprise Linux").map(&:id),
+        :template            => File.read("#{ForemanKatelloEngine::Engine.root}/app/views/unattended/kickstart-katello_rhel.erb"))
     
     ConfigTemplate.where(:name => "subscription_manager_registration").first_or_create!(
       :snippet  => true,
